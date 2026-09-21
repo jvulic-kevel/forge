c38727a147d1ba46f27f94db555e92b8a7b2b7cb
Josip Vulic <jvulic@kevel.com>
2026-09-21 15:41:01 +1000

    add(git): enable lfs config

--- modules/git.nix
+++ modules/git.nix
@@ -25,6 +25,7 @@ in
     home-manager.users.jvulic = { ... }: {
       programs.git = {
         enable = true;
+        lfs.enable = true;
         signing = {
           key = "~/.ssh/id_ed25519.pub";
           signByDefault = true;
